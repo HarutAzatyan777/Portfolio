23b5bc1f126728a5336e03baa583c1d084ec8ef0
Vahagn Melikyan <vahagn@arevakn.local>
2022-06-21 14:35:38 +0400

    first commit

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                   
+                <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+               
+                <a href="https://twitter.com/?lang=en" class="fa fa-twitter"></a>   
+                <a href="https://www.youtube.com/" class="fa fa-youtube"></a>   
+                <a href="https://www.instagram.com/" class="fa fa-instagram"></a>   
+                <a href="https://web.telegram.org/k/" class="fa fa-telegram"></a>   
+              
+                    
+                </div>
+                <div className='profile-detalils-name'>
+                    <span className='primary-text'>
+                        {""}
+                        Hello, I'M <span className='highlighed-text'>Harut</span>
+                        </span>
+                </div>
+                </div>
+            
+        </div>
+    </div>
+  )
+}
+
